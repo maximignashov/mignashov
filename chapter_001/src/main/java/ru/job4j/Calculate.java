2fea3527f02ee0856aa036fc160fa401ac0a1ac4
maximignashov <ignashov.m@icloud.com>
2017-03-21 06:57:43 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
 
+/**
+* Class Класс для вывода строки в консоль.
+* @author mignashov
+* @since 20.03.2017
+* @version 1
+*/
 public class Calculate {
+	/**
+	* Конструктор, вывод строки в консоль.
+	* @param arg - arg.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
