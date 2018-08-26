32d0e5577742f02f2f26d89a315123cd833e239e
Maxim Ignashov <ignashov.m@icloud.com>
2018-08-26 21:14:27 +0300

    #10036 Сортировка User с использованием Comparator

--- chapter_003/src/main/java/sort/SortUser.java
+++ chapter_003/src/main/java/sort/SortUser.java
@@ -1,9 +1,7 @@
 package sort;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import javax.jws.soap.SOAPBinding;
+import java.util.*;
 
 /**
  * Package for class .
@@ -15,9 +13,35 @@ import java.util.TreeSet;
 public class SortUser {
     public Set<User> sort(List<User> list) {
         Set<User> treeSet = new TreeSet<>();
-        for (User usrlst: list) {
-            treeSet.add(new User(usrlst.getAge(), usrlst.getName()));
+        for (User usrLst: list) {
+            treeSet.add(new User(usrLst.getAge(), usrLst.getName()));
         }
         return treeSet;
+    }
+    public List<User> sortNameLength (List<User> list) {
+        List<User> userList = new LinkedList<>();
+        userList.addAll(list);
+        userList.sort(new Comparator<User>() {
+            public int compare(User o1, User o2) {
+                return o1.getName().length() - o2.getName().length();
+            }
+        });
+        return userList;
+    }
+    public List<User> sortByAllFields (List<User> list) {
+        List<User> userList = new LinkedList<>();
+        userList.addAll(list);
+        userList.sort(new Comparator<User>() {
+            public int compare(User o1, User o2) {
+                int nameSort = o1.getName().compareTo(o2.getName());
+                return nameSort != 0 ? nameSort : Integer.compare(o1.getAge(), o2.getAge());
+            }
+        });
+//        userList.sort(new Comparator<User>() {
+//            public int compare(User o1, User o2) {
+//                return o1.getAge() - o2.getAge();
+//            }
+//        });
+        return userList;
     }
 }
--- chapter_003/src/test/java/sort/SortUserTest.java
+++ chapter_003/src/test/java/sort/SortUserTest.java
@@ -29,4 +29,35 @@ public class SortUserTest {
 
         assertThat(((User) currentTreeSet.toArray()[0]).getAge(), is(11));
     }
+    @Test
+    public void whenSortNameLength() {
+        List<User> usersList = new LinkedList<>();
+        usersList.add(new User(11, "Ivanov"));
+        usersList.add(new User(15, "Maxim"));
+        usersList.add(new User(14, "Igor"));
+        usersList.add(new User(11, "Evgeniy"));
+
+        SortUser sortUser = new SortUser();
+
+        List<User> currentList = sortUser.sortNameLength(usersList);
+
+        assertThat(currentList.get(0).getName(), is("Igor"));
+    }
+    @Test
+    public void whenSortByAllFields() {
+        List<User> usersList = new LinkedList<>();
+        usersList.add(new User(25, "Сергей"));
+        usersList.add(new User(30, "Иван"));
+        usersList.add(new User(20, "Сергей"));
+        usersList.add(new User(25, "Иван"));
+
+        SortUser sortUser = new SortUser();
+
+        List<User> currentList = sortUser.sortByAllFields(usersList);
+
+        assertThat(currentList.get(0).getName(), is("Иван"));
+        assertThat(currentList.get(0).getAge(), is(25));
+        assertThat(currentList.get(1).getName(), is("Иван"));
+        assertThat(currentList.get(1).getAge(), is(30));
+    }
 }
